871b9ea735e2457041f506cd535399234d036601
Young-Griff <105178807+Young-Griff@users.noreply.github.com>
2025-11-12 11:43:23 -0500

    Update App.tsx
    - Built out board.
    - Added states for player turn and board status.

--- tic-tac-toe-app/src/App.tsx
+++ tic-tac-toe-app/src/App.tsx
@@ -20,18 +20,47 @@ function BoardRow({ row, values, handleClick }) {
   );
 }
 
-function Board() {
+function Board({ player, switchPlayer }) {
   // store each squares status in the parent
   const [squares, setSquares] = useState(Array(9).fill(''));
+  let alert = null;
   
   function handleClick(i) {
-    const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
-    setSquares(nextSquares);
+    // only allow players to change empty cells
+    if (squares[i] == '') {
+      const nextSquares = squares.slice();
+      nextSquares[i] = player;
+      setSquares(nextSquares);
+      if (solved()) console.log("Solved by " + player);
+      switchPlayer();
+    }
+    else alert = null;
+  }
+
+  function solved() {
+
+    // check rows
+    for (let i = 0; i < 9; i += 3) {
+      if (squares[i] != '' && squares[i] == squares[i + 1] 
+        && squares[i] == squares[i + 2]) return true;
+    }
+
+    // check columns
+    for (let i = 0; i < 3; i++) {
+      if (squares[i] != '' && squares[i] == squares[i + 3] 
+        && squares[i] == squares[i + 6]) return true;
+    }
+
+    // check diagonals
+    if (squares[0] != '' && squares[0] == squares[4] && squares[0] == squares[8]) return true;
+    if (squares[2] != '' && squares[2] == squares[4] && squares[0] == squares[6]) return true;
+    
+    return false;
   }
 
   return (
     <>
+      { alert }
       <BoardRow row={0} values={squares.slice(0,3)} handleClick={handleClick}/>
       <BoardRow row={1} values={squares.slice(3,6)} handleClick={handleClick}/>
       <BoardRow row={2} values={squares.slice(6,9)} handleClick={handleClick}/>
@@ -40,7 +69,17 @@ function Board() {
 }
 
 function App() {
-  return <Board />;
+
+  const [player, setPlayer] = useState('X');
+
+  function switchPlayer() {
+
+    if (player == 'X') setPlayer('O');
+    else setPlayer('X');
+
+  }
+
+  return <Board player={player} switchPlayer={switchPlayer}/>;
 }
 
 export default App
